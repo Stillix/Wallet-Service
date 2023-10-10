7916a2508731dac6ddee4ef8b17ea87df49851f4
Darahakupets <ilyadorogokupets@yahoo.com>
2023-10-10 18:18:56 +0300

    input data

--- src/main/java/by/dorogokupets/walletservice/application/Main.java
+++ src/main/java/by/dorogokupets/walletservice/application/Main.java
@@ -24,7 +24,6 @@ public class Main {
 				DisplayMenu displayMenu = new DisplayMenuImpl();
 				String login = null;
 
-
 				boolean isRunning = true;
 				while (isRunning) {
 						displayMenu.showMainMenu();
